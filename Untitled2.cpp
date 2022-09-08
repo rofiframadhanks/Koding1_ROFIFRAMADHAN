fbdc0568e4b7b371d90f4ebb30a83bcafc236563
rofiframadhanks <112569173+rofiframadhanks@users.noreply.github.com>
2022-09-08 10:29:42 +0700

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int p;
+	int luas;
+	cout<<"masukkan panjang sisi persegi: ";
+	cin>>p;
+	luas=p*p;
+	cout<<"luas persegi panjang adalah: "<<luas;
+
+	int y;
+	int keliling;
+	cout<<"\nmasukkan panjang sisi persegi: ";
+	cin>>y;
+	keliling=y*4;
+	cout<<"keliling persegi adalah: "<<keliling;
+	
+	return 0;
+}
